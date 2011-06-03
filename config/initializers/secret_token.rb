4da24dd828511c824aaff27d32f19bd515037e57
Alex Bredariol Grilo <abgrilo@gmail.com>
2011-06-03 20:14:49 -0300

    V0 do componente

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Componente2::Application.config.secret_token = 'c4f63d12939438a0b4670053d407c957c1fba4a01d5167636b1149089ecec2424387e9c76a3daf1f46ec4324f7306087f2d94916e10e60d37a7c2fe22e947c2f'
